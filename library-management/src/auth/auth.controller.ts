8ff300c8c51776432887b78caf6ebf8c4f307a58
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-07-02 12:32:47 +0545

    Decorator

--- library-management/src/auth/auth.controller.ts
+++ library-management/src/auth/auth.controller.ts
@@ -1,14 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   register(@Body() dto: RegisterDto) {
@@ -18,5 +18,11 @@ export class AuthController {
   @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  profile(@CurrentUser() user) {
+    return user;
   }
 }
--- library-management/src/auth/guards/roles.guards.ts
+++ library-management/src/auth/guards/roles.guards.ts
@@ -1,7 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { UserRole } from 'src/users/user.types';
-import { ROLES_KEY } from '../roles.decorator';
+import { ROLES_KEY } from '../../common/decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {

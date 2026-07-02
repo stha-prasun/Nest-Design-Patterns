f537b698d02eb4cc548460edb6f83a599fcf8bf8
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-07-02 15:15:41 +0545

    Auth Guards

--- library-management/src/books/books.controller.ts
+++ library-management/src/books/books.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/user.types';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   create(@Body() dto: CreateBookDto) {
     return this.booksService.create(dto);
   }
--- library-management/src/books/books.module.ts
+++ library-management/src/books/books.module.ts
@@ -6,9 +6,10 @@ import { BookFactory } from './factories/book.factory';
 import { TypeOrmBookRepository } from './repositories/typeorm-book.repository';
 import { BOOK_REPOSITORY } from './constants/book.constants';
 import { BooksController } from './books.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book]), AuthModule],
 
   providers: [
     BooksService,
